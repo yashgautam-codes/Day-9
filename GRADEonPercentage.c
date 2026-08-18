b836fb11829fde7342f891b0487fd30693b1e604
yashgautam-codes <yashgautam8368@gmail.com>
2026-08-18 11:20:34 +0530

    Add grading criteria comments to GRADEonPercentage.c
    Added comments to explain grading criteria based on percentage.

--- GRADEonPercentage.c
+++ GRADEonPercentage.c
@@ -1,3 +1,9 @@
+// Q18. Write a program to accept a percentage (0-100) and assign a grade based on the following criteria :
+//        90-100:Grade A
+//        80-89:Grade B
+//        70-79:Grade C
+//        60-69:Grade D
+//        below 60:Grade F
 #include <stdio.h>
 int main()
 {
