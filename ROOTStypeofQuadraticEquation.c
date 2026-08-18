9d633e72697f178a684f02a74090c8559a43242d
yashgautam-codes <yashgautam8368@gmail.com>
2026-08-18 11:31:05 +0530

    Add files via upload

---
+++ ROOTStypeofQuadraticEquation.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter 'a' :- ");
+    scanf("%d",&a);
+    int b;
+    printf("Enter 'b' :- ");
+    scanf("%d",&b);
+    int c;
+    printf("Enter 'c' :- ");
+    scanf("%d",&c);
+    int D = (((b*b)-4*a*c));
+    if(D>0)
+    {
+        printf("Root's are Real & Different: %d,%d",a,c);
+    }
+    if(D==0)
+    {
+        printf("Root's are Equal: %d",a);
+    }
+    if(D<0)
+    {
+        printf("Root's are Complex");
+    }
+    return 0;
+}
